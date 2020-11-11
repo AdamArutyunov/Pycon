4f4fea9c6b7306d015572178922085b3f5aeefe5
Adam Arutyunov <arutyunov.adam@yandex.ru>
2020-11-11 16:18:15 +0000

    Labours duration to end date

--- forms/create_labour.py
+++ forms/create_labour.py
@@ -7,6 +7,6 @@ from wtforms.fields.html5 import DateTimeLocalField
 class CreateLabourForm(FlaskForm):
     labour_name = StringField('Название')
     start_date = DateTimeLocalField('Начало', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
-    duration = IntegerField('Длительность')
+    end_date = DateTimeLocalField('Конец', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
     perfomance_time = IntegerField('Время на решение')
     submit = SubmitField('Создать')
